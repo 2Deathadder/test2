6c0f7e214f6af3499fe3eb6adc68752a25995663
2Deathadder <3thomwinner2007@gmail.com>
2026-08-09 23:17:59 +0000

    Mise à jour via génération IA 21st.dev : src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 import App from './App';
-import './styles.css';
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode><App /></React.StrictMode>
+);
